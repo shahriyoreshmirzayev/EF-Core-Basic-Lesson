8ac72a7dff14f5f9f4841316c80ac8579627b27f
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2024-12-28 19:12:17 +0500

    Databasedan ma'lumot olish

--- Entity Framework Coreda amaliyot/Program.cs
+++ Entity Framework Coreda amaliyot/Program.cs
@@ -7,11 +7,32 @@ namespace Entity_Framework_Coreda_amaliyot
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
-            Query();
+            //Query();
+            QuerySubject();
         }
         public static void Query()
         {
+            // Vaqtni qabul qilmasa qabulqildirish kodi
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
             EkundalikContext context = new EkundalikContext();
+            Subject English = new();
+
+            English.SubjectName = "Dasturlash";
+            context.Subjects.Add(English);
+            context.SaveChanges();
+            Console.WriteLine("Amaliyot bajarildi");
+            Console.Read();
+        }
+        public static void QuerySubject()
+        {
+            EkundalikContext context = new EkundalikContext();
+            List<Subject> list = context.Subjects.ToList();
+
+            foreach (Subject subject in list)
+            {
+                Console.WriteLine(subject.SubjectName);
+            }
+            Console.Read();
         }
     }
 }
